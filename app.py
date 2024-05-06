bc15b1b7ddfcd69a2797b38b1aec0ec88da578ad
lpmu06 <lpmu06@gmail.com>
2024-05-06 01:07:44 -0300

    Commit inicial

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+
+# Sidebar contents
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Lpzinho do amor)')
+
+
+def main():
+    st.header("Chat with PDF =D")
+
+    load_dotenv() 
+
+    #Upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    # st.write(pdf)
+    
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+
+        # embeddings
+        embeddings = OpenAIEmbeddings()
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        store_name = pdf.name[:-4]
+        with open(f"{store_name}.pkl", "wb") as f:
+            pickle.dump(VectorStore, f)
+
+
+        #st.write(text)
+
+if __name__ == '__main__':
+    main()
---
+++ readme.txt
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=RIWbalZ7sTo&ab_channel=PromptEngineering
+https://pastebin.com/mcHG4cY4
+
+https://www.youtube.com/watch?v=dXxQ0LR-3Hg&ab_channel=AlejandroAO-Software%26Ai
+https://github.com/alejandro-ao/ask-multiple-pdfs/blob/main/requirements.txt
