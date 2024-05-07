25ae4cc55148b447e8085edb11908f84ff9abde3
Luis Paulo Maia Uchôa <lpmu06@gmail.com>
2024-05-07 15:36:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os 
 
 
 # Sidebar contents
@@ -20,7 +24,8 @@ with st.sidebar:
  
     ''')
     add_vertical_space(5)
-    st.write('Made with ❤️ by [Lpzinho do amor)')
+    #st.write('Made with ❤️ by Lpzinho do amor')
+    st.write('Made by **LP**')
 
 
 def main():
@@ -45,13 +50,36 @@ def main():
             length_function=len
         )
         chunks = text_splitter.split_text(text=text)
-
-        # embeddings
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        #st.write(chunks)
+        
+        #embeddings
         store_name = pdf.name[:-4]
-        with open(f"{store_name}.pkl", "wb") as f:
-            pickle.dump(VectorStore, f)
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl","rb") as f:
+                VectorStore = pickle.load(f)
+            #st.write("Embeddings Loeaded from the Disk")
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+            #st.write("Embeddings Computation Completed")
+
+        #Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+        #st.write(query)
+
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+            #st.write(docs)
+            llm = OpenAI(temperature=0.7, model_name= "gpt-3.5-turbo")
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response =chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response) 
+
 
 
         #st.write(text)
